d6c399519a33e5864496e7a406d2127fdb1a7e41
MacBook <macbook@macbookpro.kpn>
2024-11-03 23:06:59 +0100

    Drainer / Transfer / Permit / Permit2

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -6,12 +6,12 @@ import { useRouter } from "next/navigation";
 
 const Hero = () => {
   const router = useRouter();
-  const { scrollYProgress, scrollY } = useScroll();
+  const { scrollY } = useScroll();
   const [screenHeight, setScreenHeight] = React.useState(0);
 
   React.useEffect(() => {
-    const screenHeight = window?.innerHeight;
-    setScreenHeight(screenHeight);
+    // Set screen height for animations
+    setScreenHeight(window.innerHeight);
   }, []);
 
   // Calculate initial values for y1 and y2 based on the scroll position
@@ -23,10 +23,7 @@ const Hero = () => {
   const y2 = useTransform(scrollY, [0, screenHeight], [0, initialY2]);
 
   return (
-    <section
-      className="relative z-10 flex h-[100dvh] w-full flex-col overflow-hidden bg-primary-light px-10 py-10 dark:bg-primary-dark
-xl:flex-row "
-    >
+    <section className="relative z-10 flex h-[100vh] w-full flex-col overflow-hidden bg-primary-light px-10 py-10 dark:bg-primary-dark xl:flex-row">
       <motion.article
         style={{ translateY: y1 }}
         className="z-10 flex h-full flex-col items-center justify-center text-center md:items-start md:justify-start md:text-left"
@@ -52,20 +49,20 @@ xl:flex-row "
         style={{ translateY: y2 }}
         className="z-10 hidden h-full w-full flex-col justify-end gap-5 pl-10 md:flex xl:gap-10 xl:pl-0"
       >
-        <article className="  flex w-full justify-end ">
-          <p className="max-w-[25rem] md:max-w-[35rem] rounded-lg md:text-xs  border border-primary-light bg-primary-dark p-4 text-[0.6rem] text-primary-light dark:border-primary-dark dark:bg-primary-light dark:text-primary-dark xl:py-2 xl:max-w-[45rem] 2xl:text-base">
+        <article className="flex w-full justify-end">
+          <p className="max-w-[25rem] md:max-w-[35rem] rounded-lg md:text-xs border border-primary-light bg-primary-dark p-4 text-[0.6rem] text-primary-light dark:border-primary-dark dark:bg-primary-light dark:text-primary-dark xl:py-2 xl:max-w-[45rem] 2xl:text-base">
             In the ever-evolving landscape of blockchain and cryptocurrency,
             efficiency, convenience, and innovation are paramount.
           </p>
         </article>
-        <article className="flex w-full justify-end  ">
-          <p className="max-w-[15rem] md:max-w-[25rem] rounded-lg md:text-xs  border border-primary-light bg-primary-dark p-4 text-[0.6rem] text-primary-light dark:border-primary-dark dark:bg-primary-light dark:text-primary-dark xl:py-2 xl:max-w-[35rem] 2xl:text-base">
+        <article className="flex w-full justify-end">
+          <p className="max-w-[15rem] md:max-w-[25rem] rounded-lg md:text-xs border border-primary-light bg-primary-dark p-4 text-[0.6rem] text-primary-light dark:border-primary-dark dark:bg-primary-light dark:text-primary-dark xl:py-2 xl:max-w-[35rem] 2xl:text-base">
             Aikisend is a groundbreaking solution that re-imagines the way
             digital assets are transferred.
           </p>
         </article>
-        <article className="  flex w-full justify-end">
-          <p className="max-w-[30rem] md:max-w-[40rem] rounded-lg md:text-xs  border border-primary-light bg-primary-dark p-4 text-[0.6rem] text-primary-light dark:border-primary-dark dark:bg-primary-light dark:text-primary-dark xl:py-2 xl:max-w-[50rem] 2xl:text-base">
+        <article className="flex w-full justify-end">
+          <p className="max-w-[30rem] md:max-w-[40rem] rounded-lg md:text-xs border border-primary-light bg-primary-dark p-4 text-[0.6rem] text-primary-light dark:border-primary-dark dark:bg-primary-light dark:text-primary-dark xl:py-2 xl:max-w-[50rem] 2xl:text-base">
             It provides an unprecedented level of flexibility and simplicity to
             users seeking to send multiple tokens to multiple addresses in a
             single transaction.
